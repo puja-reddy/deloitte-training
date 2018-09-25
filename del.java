7e885c96cefea69c9436cc39472f373ec78049f2
puja-reddy <43532693+puja-reddy@users.noreply.github.com>
2018-09-25 16:55:48 +0530

    Add files via upload

---
+++ del.java
@@ -0,0 +1,28 @@
+class del
+{
+public void ne()
+{
+System.out.println(" welcome to deloitte ");
+del d=new del();
+d.dis();
+}
+public void dis()
+{
+System.out.println("ovver");
+}
+public static void main(String a[])
+{
+System.out.println("  w2d means");
+del d=new del();
+d.ne();
+bye b=new bye();
+b.sayTata();
+}
+}
+class bye
+{
+public void sayTata()
+{
+System.out.println("bye");
+}
+}
